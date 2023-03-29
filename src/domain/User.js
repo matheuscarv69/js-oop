e91720afe6cbcce5a83d4cdd463a19a9d1de65ce
matheuscarv69 <matheus9126@gmail.com9>
2023-03-28 20:58:33 -0400

    :sparkles: feat: implementing encapsulation in others  User properties

--- src/domain/User.js
+++ src/domain/User.js
@@ -1,17 +1,21 @@
 export default class User {
   // Implementing encapsulation in name property
   #name
+  #email
+  #birthDate
+  #role
+  #active
 
   constructor(name, email, birthDate, role, active = true) {
     this.#name = name
-    this.email = email
-    this.birthDate = birthDate
-    this.role = role || 'student'
-    this.active = active
+    this.#email = email
+    this.#birthDate = birthDate
+    this.#role = role || 'student'
+    this.#active = active
   }
 
   showInfos() {
-    return `${this.#name}, ${this.email}`
+    return `${this.#name}, ${this.#email}`
   }
 
 }
--- src/index.js
+++ src/index.js
@@ -4,8 +4,3 @@ import Teacher from "./domain/Teacher.js"
 
 const newUser = new User('Marian',  'm@m.com', '2021-01-01')
 console.log(newUser.showInfos())
-
-// This is allowed because properties of User class not implementing
-// encapsulation
-newUser.#name = 'Julia'
-console.log(newUser.name)
